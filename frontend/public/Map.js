914e31f0e035fe5991812c091603e72fe835d886
torichoudhury <torichoudhury@gmail.com>
2025-04-28 15:47:20 +0530

    git ignore

--- frontend/public/Map.js
+++ frontend/public/Map.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GoogleMap, LoadScript, DirectionsService, DirectionsRenderer } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '800px',
+  height: '500px'
+};
+
+const center = {
+  lat: 19.0760, // Mumbai
+  lng: 72.8777
+};
 
 function Map() {
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+
+  const pickup = "Mumbai Central"; // you can pass via props later
+  const destination = "Bandra Kurla Complex"; // you can pass via props later
+
+  const directionsCallback = (response) => {
+    if (response !== null && response.status === 'OK') {
+      setDirectionsResponse(response);
+    } else {
+      console.log('Response: ', response);
+    }
+  };
+
   return (
-    <div style={{ marginTop: "20px" }}>
-      <h2>Map will be displayed here.</h2>
-      {/* Later integrate Google Maps */}
-    </div>
+    <LoadScript googleMapsApiKey="YOUR_GOOGLE_MAPS_API_KEY">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={12}
+      >
+        <DirectionsService
+          options={{
+            destination: destination,
+            origin: pickup,
+            travelMode: 'DRIVING'
+          }}
+          callback={directionsCallback}
+        />
+
+        {directionsResponse && (
+          <DirectionsRenderer
+            options={{
+              directions: directionsResponse
+            }}
+          />
+        )}
+      </GoogleMap>
+    </LoadScript>
   );
 }
 
